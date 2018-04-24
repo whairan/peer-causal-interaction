854fe00c160e638ff989d0cce6bd8d5dbde4c932
William Tarimo <wtarimo@brandeis.edu>
2018-04-23 22:56:13 -0400

    Completed users/show template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,20 @@ class UsersController < ApplicationController
 
 	
 	def show
-		sevenPM = DateTime.now.change(:hour=>19, :minute => 0, :second=>0)
+		@sevenPM = Time.now.change(:hour=>19, :minute => 0, :second=>0)
+		@tenPM = Time.now.change(:hour=>22, :minute => 0, :second=>0)
+		@todays_report = nil
+		@todays_feedbacks = []
 		if @user.self_reporter
-			tenPM = DateTime.now.change(:hour=>22, :minute => 0, :second=>0)
-			@todays_report = Report.where(:user_id=>@user.id,:created_at=>sevenPM..tenPM).first
+			@todays_report = Report.where(:user_id=>@user.id,:created_at=>@sevenPM..@tenPM).first
 			if @todays_report
 				@feedback = @todays_report.feedback
 			end
 		else
-			elevenPM = DateTime.now.change(:hour=>23, :minute => 0, :second=>0)
-			@todays_feedbacks = Feedback.where(:user_id=>@user.id,:created_at=>sevenPM..elevenPM)
+			@elevenPM = Time.now.change(:hour=>23, :minute => 0, :second=>0)
+			@todays_feedbacks = Feedback.where(:user_id=>@user.id,:created_at=>@sevenPM..@elevenPM)
 		end
+		
 	end
 
 
