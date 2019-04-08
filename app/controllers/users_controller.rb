f118e9d7a3f8e2d3088d1ec151326113c8ef65c2
William Tarimo <wtarimo@conncoll.edu>
2019-04-08 16:58:30 -0400

    Added comments in feedback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
 	
 	def show
-		@sevenPM = Time.zone.now.change(:hour=>16, :minute => 0, :second=>0)
+		@sevenPM = Time.zone.now.change(:hour=>18, :minute => 0, :second=>0)
 		@tenPM = Time.zone.now.change(:hour=>22, :minute => 0, :second=>0)
 		@todays_report = nil
 		@todays_feedbacks = []
