61c00d46c451c3e0455af0ea58d01e8a897bc2db
William Tarimo <wtarimo@conncoll.edu>
2019-04-29 17:56:23 -0400

    Added prevoius report/feecback and reports dictionary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,24 @@ class UsersController < ApplicationController
 				@feedback = @todays_report.feedback
 			end
 			@latest_report = Report.where(:user_id=>@user.id).last
+
+
+
+			@reports = {}
+			all_reports = Report.where(user_id:@user.id)
+			all_reports.each do |report|
+				date = report.created_at.strftime("%m/%d/%Y")
+				report_info = {attendance:report.attendance,assignments:report.assignments,participation:report.participation,
+					sleep:report.sleep,exercise:report.exercise,meals:report.meals}
+				feedback_info = {}
+				feedback = report.feedback
+				if feedback
+					feedback_info = {attendance:feedback.attendance,assignments:feedback.assignments,participation:feedback.participation,
+					sleep:feedback.sleep,exercise:feedback.exercise,meals:feedback.meals,comments:feedback.comments}
+				end
+				@reports[date] = {report:report_info,feedback:feedback_info}
+			end
+
 		else
 			@elevenPM = Time.zone.now.change(:hour=>23, :minute => 0, :second=>0)
 			@todays_feedbacks = Feedback.where(:user_id=>@user.id,:created_at=>@sevenPM..@elevenPM)
